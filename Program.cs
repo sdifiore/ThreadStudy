84ae157a67b68c1e18ea0c1ac30ddc3fb027d7d6
sdifiore <difiores@outlook.com>
2022-07-18 17:53:47 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿public class MainClass
+{
+    private const int REPETITIONS = 100;
+
+    public static void DoWork()
+    {
+        for (int i = 0; i < REPETITIONS; i++)
+        {
+            Console.Write("B");
+        }
+    }
+
+    public static void Main()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            Thread t = new Thread(DoWork);
+            t.Name = $"Thread {i}";
+            t.Start();
+        }
+
+        int dummy = 123;
+    }
+}

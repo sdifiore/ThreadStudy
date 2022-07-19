1ffa14e40780a6b17e882ce0299d41ef0740678c
sdifiore <difiores@outlook.com>
2022-07-19 10:38:18 -0300

    Race Condition Same Variable

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿public class MainClass
 {
-    private const int REPETITIONS = 100;
+    public static int i = 0;
 
     public static void DoWork()
     {
-        for (int i = 0; i < 5; i++)
+        for (i = 0; i < 5; i++)
         {
             Console.WriteLine($"{i} *");
         }
